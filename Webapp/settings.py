0009cdb14c5f7baa78eded4286ed278943eaff06
Alex Chee <xxchee555xx@gmail.com>
2023-02-19 19:00:05 +0800

    Added secret key to env

--- Webapp/settings.py
+++ Webapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3!u-fdd0jb)0d+ekmfxrg+#zr-)c!q6*+_*1n&9*$k8=x&7-g="
+with open('.env') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
