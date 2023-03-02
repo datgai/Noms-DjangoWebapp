75e2a92f085d98e9fa82dd8be21d39373dc59e88
Alex Chee <xxchee555xx@gmail.com>
2023-03-03 00:32:26 +0800

    Added basic tests

--- foodie/tests.py
+++ foodie/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class Users_model_Test(TestCase):
+    def setUp(self):
+        User.objects.create(username="testUser")
+    
+    def test_user_exists(self):
+        testUser = User.objects.get(username="testUser")
+
